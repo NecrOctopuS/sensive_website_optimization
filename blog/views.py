86c61aeb2f81c431e7460a520ec0dd7af36947e2
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2019-11-30 14:55:20 +0300

    Show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -27,9 +27,13 @@ def serialize_tag(tag):
     }
 
 
-def index(request):
+def get_likes_count(post):
+    return len(post.likes.all())
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+
+def index(request):
+    all_posts = Post.objects.all()
+    most_popular_posts = sorted(all_posts, key=get_likes_count)[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -111,3 +115,5 @@ def contacts(request):
     # позже здесь будет код для статистики заходов на эту страницу
     # и для записи фидбека
     return render(request, 'contacts.html', {})
+
+
